59cb5a783c56e79faa9882c77d94c7c7697cf749
Eviera16 <64331763+Eviera16@users.noreply.github.com>
2022-12-04 15:37:09 -0500

    connect database

--- database.sql
+++ database.sql
@@ -5,5 +5,6 @@ CREATE TABLE galleryImageObjs
     galleryImageObj_id SERIAL PRIMARY KEY,
     title VARCHAR(225),
     description VARCHAR(225),
-    image VARCHAR(225)
+    image VARCHAR(225),
+    imgbuffer TEXT
 );
--- db.js
+++ db.js
@@ -1,11 +1,19 @@
 const Pool = require('pg').Pool;
 
+// const pool = new Pool({
+//     user: "postgres",
+//     password: "#E16Vev!",
+//     host: "localhost",
+//     port: 5432,
+//     database: "clutchdatabase"
+// });
+
 const pool = new Pool({
-    user: "postgres",
-    password: "#E16Vev!",
-    host: "localhost",
+    user: "dxfdcubtwzpnrh",
+    password: "95109ac28212ef8382d21a135221e082a84f3466fde7c4a27c8417dc69840bd4",
+    host: "ec2-3-223-169-166.compute-1.amazonaws.com",
     port: 5432,
-    database: "clutchdatabase"
+    database: "d4mmlh8phneco9"
 });
 
 module.exports = pool;
