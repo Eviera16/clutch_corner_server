e2c433230eb395a505856e8d8b6b98765140cd8a
Eviera16 <64331763+Eviera16@users.noreply.github.com>
2022-12-04 15:53:53 -0500

    port and Procfile

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const multer = require('multer');
 const bodyParser = require('body-parser');
 const pool = require('./db');
 const app = express();
+const port = process.env.PORT || 4000
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
@@ -163,4 +164,4 @@ app.get("/api/getGView", (req, res) => {
     res.json(galleryViewImage);
 })
 
-app.listen(4000, () => { console.log("Server listening on port 4000.") })
+app.listen(port, () => { console.log("Server listening on port 4000.") })
